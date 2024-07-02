89342fc11f9f437e7e587ca57e006459b2cce6ea
Mikkel <MikkelMM99@gmail.com>
2024-07-02 12:56:32 +0200

    Initial commit for running on lxplus

---
+++ job_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script is used to run multiple jobs, for benchmarking purposes in sequence
+#conda activate tvm-build
+# Missing tvm support conv1d_rnn, convlstm1d, gru, rnn, stacked_gru, stacked_rnn
+for network in conv1d_rnn conv1d conv2d convlstm1d depthwise_conv1d gru lstm mlp_conv mlp_deep_wide mlp_deep mlp_piecewise mlp_repeat mlp_wide mlp rnn separable_conv1d separable_conv2d stacked_gru stacked_rnn 
+do
+    for target in llvm opencl cuda
+    do
+        for batch_size in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768
+        do
+            python benchmark.py --network=$network --target=$target --batch_size=$batch_size
+        done
+    done
+done
---
+++ training_and_conversion/training/models_tf/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["MLP", "qMLP", "RNN","tfProbability", "models"]
