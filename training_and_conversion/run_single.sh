51c048968fff5922eed95bf2bcefc9b69d66c0d6
Mikkel Moller Modekjaer <mimodekj@lxplus985.cern.ch>
2024-07-07 10:51:58 +0200

    Fixed job submission

--- training_and_conversion/run_single.sh
+++ training_and_conversion/run_single.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python training/training.py --windowSize 3 --epoch 100 --batch 2048 --modelName mlp --modelType MLP
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install tensorflow[and-cuda]==2.15.* apache_beam tensorflow_datasets
+python3 training/training.py --windowSize 3 --epoch 100 --batch 2048 --modelName mlp --modelType MLP
